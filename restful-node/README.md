664baeb288d48f380505c28bb2548043ac97bb33
NelsonRock <nelsao101@gmail.com>
2016-06-02 14:13:09 +0200

    Refactoring and readme changes

--- restful-node/README.md
+++ restful-node/README.md
@@ -1,1 +1,15 @@
-Exploring restfull-services in node
+> Exploring restfull-services in node with Es6 import and export module
+```
+npm install --save-dev babel-cli- babel-core-babel-preset-es2015 nodemon
+
+```
+Config your package.json to:
+```
+"scripts": {
+  "dev": "nodemon --exec babel-node src/index.js",
+  "prestart": "babel src --out-dir dist",
+  "start": "node dist/index.js"
+},
+```
+
+> And if you want install curl for testing!
--- restful-node/src/index.js
+++ restful-node/src/index.js
@@ -1,13 +1,21 @@
 import {createServer} from 'http';
 import {sumTwo, mult } from '../modules/math/addition';
+import _array from 'lodash/array';
+
 console.log("Suma:" + sumTwo(6, 7));
 console.log("Mult:" + mult(6, 7));
+
+var number = _array.indexOf([1, 2, 3, 4, 5, 6], 3);
+console.log("Encontrado:" + number);
+
 
 var url = require('url');
 let items = [];
 createServer((req, res)=>{
   console.log(items);
   const path = url.parse(req.url).pathname; // pathname: '/1'
+  console.log("Path:" + path );
+
   let i = '';
   switch(req.method){
     case 'POST':
@@ -25,6 +33,12 @@ createServer((req, res)=>{
       break;
 
   case 'GET':
+    i = parseInt(path.slice(1), 10);
+    console.log("Parsed:" + i);
+
+    if(i){
+      console.log("Item finded:" + items[i]);
+    }
     let body = items.map((v, i)=>{
     return i + ')' + v+ '\n' ;
   }).join('\n');
@@ -35,6 +49,10 @@ createServer((req, res)=>{
 
   case 'PUT':
     i = parseInt(path.slice(1), 10);
+    console.log("Parsed PUT:" + i);
+    let number = _array.indexOf(items, i);
+    console.log("Put method find item:" + items[number] );
+
     if(isNaN(i)){
       res.statusCode = 400 //item is not valid, not a number
       res.end('Invalid item id');
